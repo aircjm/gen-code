cb09419e0d07d1fc28e14fa301dcd156a72934e3
aircjm <aircjm@gmail.com>
2022-09-30 17:35:04 +0800

    feat(datasource): form submit

--- semi-vite/src/pages/datasource/Edit.tsx
+++ semi-vite/src/pages/datasource/Edit.tsx
@@ -18,14 +18,22 @@ function DatasourceEdit() {
                 {({formState, values, formApi}) => (
                     <>
                         <Form.Input field='name' label='名称' style={{width: '100%'}}></Form.Input>
-                        <Form.Input field='url' label='jdbcUrl' style={{width: '100%'}}></Form.Input>
+                        <Form.TextArea field='desc' label='描述' style={{width: '100%'}}></Form.TextArea>
+                        <Form.Input field='url' label='jdbcUrl链接' style={{width: '100%'}}></Form.Input>
                         <Form.Select field='dbType' label='dbType' style={{width: '100%'}}>
                             <Form.Select.Option label={"MySQL"} value={1}></Form.Select.Option>
                             <Form.Select.Option label={"PostgreSQL"} value={2}></Form.Select.Option>
                             <Form.Select.Option label={"Oracle"} value={3}></Form.Select.Option>
                         </Form.Select>
+                        <Form.Input field='ip' label='ip' style={{width: '100%'}}></Form.Input>
+                        <Form.InputNumber field='port' label='port' style={{width: '100%'}}
+                                          hideButtons={true}></Form.InputNumber>
+                        <Form.Input field='username' label='username' style={{width: '100%'}}></Form.Input>
+                        <Form.Input field='password' label='password' style={{width: '100%'}}
+                                    mode="password"></Form.Input>
+
                         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                            <Button htmlType='submit' type="tertiary">Log in</Button>
+                            <Button htmlType='submit' type="tertiary">submit</Button>
                         </div>
                     </>
                 )}
