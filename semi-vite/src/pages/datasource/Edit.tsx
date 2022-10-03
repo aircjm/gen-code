f62c31d04f7d08c93a4e0d6d1d987cbccc588d81
aircjm <aircjm@gmail.com>
2022-10-03 11:02:05 +0800

    feat(datasource): 数据库类型配置

--- semi-vite/src/pages/datasource/Edit.tsx
+++ semi-vite/src/pages/datasource/Edit.tsx
@@ -46,8 +46,9 @@ function DatasourceEdit(props: any) {
                     <Form.Input field='url' label='jdbcUrl链接' style={{width: '100%'}}></Form.Input>
                     <Form.Select field='dbType' label='dbType' style={{width: '100%'}}>
                         <Form.Select.Option label={"MySQL"} value={1}></Form.Select.Option>
-                        <Form.Select.Option label={"PostgreSQL"} value={2}></Form.Select.Option>
-                        <Form.Select.Option label={"Oracle"} value={3}></Form.Select.Option>
+                        <Form.Select.Option label={"Oracle"} value={2}></Form.Select.Option>
+                        <Form.Select.Option label={"SQL Server"} value={3}></Form.Select.Option>
+                        <Form.Select.Option label={"PostgreSQL"} value={4}></Form.Select.Option>
                     </Form.Select>
                     <Form.Input field='host' label='host' style={{width: '100%'}}></Form.Input>
                     <Form.InputNumber field='port' label='port' style={{width: '100%'}}
