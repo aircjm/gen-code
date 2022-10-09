08ea6315192e688592d89d4685a8cac571892e6c
aircjm <aircjm@gmail.com>
2022-10-09 15:43:36 +0800

    feat(gen): 生成表单

--- semi-vite/src/pages/generate/Generate.tsx
+++ semi-vite/src/pages/generate/Generate.tsx
@@ -1,4 +1,4 @@
-import {Button, Checkbox, CheckboxGroup, List, Radio, RadioGroup, Select, Space} from "@douyinfe/semi-ui";
+import {Button, Checkbox, CheckboxGroup, List, Notification, Radio, RadioGroup, Select, Space} from "@douyinfe/semi-ui";
 import React, {useEffect, useState} from "react";
 import {AxiosResponse} from "axios";
 import {generate} from "../../api/generate";
@@ -6,13 +6,37 @@ import {datasourceList} from "../../api/datasource";
 
 function Generate(props: any) {
 
-    const data = ["admin"];
+    const data = [{
+        "schema": null,
+        "tableName": "zhb_grid_seek_help",
+        "comment": "代办服务",
+        "columnDefinitions": [],
+        "csharpColumnDefinitions": [],
+        "hasDateColumn": false,
+        "hasDateField": false,
+        "hasLocalDateField": false,
+        "hasLocalDateTimeField": false,
+        "hasBigDecimalField": false,
+        "hasJsonbField": false
+    }, {
+        "schema": null,
+        "tableName": "zhb_grid_volunteer_manange",
+        "comment": "志愿者申请",
+        "columnDefinitions": [],
+        "csharpColumnDefinitions": [],
+        "hasDateColumn": false,
+        "hasDateField": false,
+        "hasLocalDateField": false,
+        "hasLocalDateTimeField": false,
+        "hasBigDecimalField": false,
+        "hasJsonbField": false
+    }];
 
     const [loading, setLoading] = useState<boolean>(false)
     const [datasourceOptionList, setDatasourceOptionList] = useState([])
 
-    const [checkboxVal, setCV] = useState([...data[0]]);
-    const [radioVal, setRV] = useState(data[0]);
+    const [checkboxVal, setCV] = useState([]);
+    const [radioVal, setRV] = useState();
 
     useEffect(() => {
         datasourceList().then((response: AxiosResponse) => {
@@ -23,22 +47,23 @@ function Generate(props: any) {
 
 
 
-    const handleSubmit = (values: any) => {
+
+    const handleSubmit = () => {
         setLoading(true);
-        generate(values).then((response: AxiosResponse) => {
-            // @ts-ignore
-            if (parseInt(response.code) === 0) {
-                setLoading(false);
-            }
+        const genParam = {
+            "tableList": checkboxVal
+        }
+        Notification.info({
+            content: JSON.stringify(genParam)
         })
     };
 
     return (
         <Space align="start" wrap spacing={20}>
-            <Button type={"primary"} onClick={handleSubmit}></Button>
+            <Button type={"primary"} onClick={handleSubmit}>生成</Button>
             <Select placeholder='请选择数据源' style={{width: 180}} optionList={datasourceOptionList}></Select>
             <div style={{ display: 'flex' }}>
-                <div style={{ marginRight: 16, width: 280, display: 'flex', flexWrap: 'wrap' }}>
+                <div style={{ marginRight: 16, width: 400, display: 'flex', flexWrap: 'wrap' }}>
                     <CheckboxGroup value={checkboxVal} onChange={(value) => setCV(value)}>
                         <List
                             dataSource={data}
@@ -46,11 +71,11 @@ function Generate(props: any) {
                             split={false}
                             size='small'
                             style={{ border: '1px solid var(--semi-color-border)', flexBasis: '100%', flexShrink: 0 }}
-                            renderItem={item => <List.Item className='list-item'><Checkbox value={item}>{item}</Checkbox></List.Item>}
+                            renderItem={item => <List.Item className='list-item'><Checkbox value={item.tableName}>{item.tableName}-{item.comment}</Checkbox></List.Item>}
                         />
                     </CheckboxGroup>
                 </div>
-                <div style={{ marginRight: 16, width: 280, display: 'flex', flexWrap: 'wrap' }}>
+                <div style={{ marginRight: 16, width: 400, display: 'flex', flexWrap: 'wrap' }}>
                     <RadioGroup value={radioVal} onChange={(e) => setRV(e.target.value)}>
                         <List
                             className='component-list-demo-booklist'
@@ -58,7 +83,7 @@ function Generate(props: any) {
                             split={false}
                             size='small'
                             style={{ border: '1px solid var(--semi-color-border)', flexBasis: '100%', flexShrink: 0 }}
-                            renderItem={item => <List.Item className='list-item'><Radio value={item}>{item}</Radio></List.Item>}
+                            renderItem={item => <List.Item className='list-item'><Radio value={item.tableName}>{item.comment}</Radio></List.Item>}
                         />
                     </RadioGroup>
                 </div>
